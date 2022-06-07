1ed664316d1086be3afd42ca732fb349c0f06b8d
Saulo <22698083+saulodigital@users.noreply.github.com>
2022-06-07 17:10:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 import discord from 'discord.js';
 import dotenv from 'dotenv';
 import axios from 'axios';
-dotenv.config();
 
+// config and setup
+dotenv.config();
 const intents = new discord.Intents(8);
 const client = new discord.Client({ intents });
 client.login(process.env.BOT_TOKEN);
 
-// set bot status for fun
+
 client.on('ready', async() => {
+
+    // set bot status for fun
     await client.user.setActivity('in the Lab.');
     console.log('All roads lead to Eco!');
-});
-
-// fetch all server roles
-client.on('ready', async () => {
-
+    
+    // fetch all server roles
     const options = {
         method: 'GET',
         url: `${process.env.DISCORD_API}/guilds/${process.env.GUILD_ID}/roles`,
